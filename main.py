32e21f23a3a9d9dfb251d13d5985deea020aaee8
JohnDroy <johncdroy@gmail.com>
2021-11-12 10:55:44 +0000

    add words list to words.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from word import word_list
+
+def get_words():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 7
+    print("Play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
