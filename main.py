8c8ee76285bb7c2e0c97cc7989ac6ddfa5e56647
JohnDroy <johncdroy@gmail.com>
2021-11-12 11:11:03 +0000

    add functions for game to detect correct and incorrect words/letters

--- main.py
+++ main.py
@@ -24,5 +24,25 @@ def play(word):
                 print(guess, "Is not in that word.")
                 tries -= 1
                 guessed_letters.append(guess)
-            else    
+            else:
+                print("Great job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list) 
+                if "_" not in word_completion:
+                    guessed = True   
+
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You have already guessed that word", guess)
+            elif guess != word:
+                print(guess, "Incorrect word")  
+        else:  
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")      
 
